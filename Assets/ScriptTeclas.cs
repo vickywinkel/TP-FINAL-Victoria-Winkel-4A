aea16ed5a0e6f021d8c8820401502a3730d9b029
vickywinkel <152075054+vickywinkel@users.noreply.github.com>
2024-06-19 09:08:31 -0300

    terminado parte 1

---
+++ Assets/ScriptTeclas.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScriptTeclas : MonoBehaviour
+{
+    public GameObject[] objetos;
+    private int counter = 0; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < objetos.Length; i++)
+        {
+            objetos[i].SetActive(false);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            objetos[counter].SetActive(false);
+            counter--;
+            objetos[counter].SetActive(true);
+        }
+        else if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+
+            objetos[counter].SetActive(false);
+            counter++;
+            objetos[counter].SetActive(true);
+        }
+    }
+}
